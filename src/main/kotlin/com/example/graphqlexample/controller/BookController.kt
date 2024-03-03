cdefa96a8cc195c2efec67fcfdf455e265c9ea33
Zach Lewis <zaclewis@gmail.com>
2024-03-03 14:54:34 -0600

    sub flux 1

--- src/main/kotlin/com/example/graphqlexample/controller/BookController.kt
+++ src/main/kotlin/com/example/graphqlexample/controller/BookController.kt
@@ -3,8 +3,12 @@ package com.example.graphqlexample.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping
+
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
+import reactor.core.publisher.Flux;
+import java.time.Duration
 
 
 @Controller
@@ -18,6 +22,12 @@ class BookController {
     @QueryMapping
     fun book(@Argument id: String): String? {
         return "Book 222 "
+    }
+
+    @SubscriptionMapping
+    fun bookchange(): Flux<Long> {
+        return Flux.interval(Duration.ofSeconds(1))
+    .map { i -> i }
     }
 
 }
